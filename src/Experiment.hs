efd898f449f410654094469b92f099653292c20a
Naren Sundaravaradan <nano.naren@gmail.com>
2010-12-26 12:43:14 +0800

    Going to move to real LCA thing.

--- src/Experiment.hs
+++ src/Experiment.hs
@@ -7,10 +7,13 @@ module Main
 import ChineseRem (divisorSearch)
 import ChineseRem.Set (run)
 import ChineseRem.IndepSet
+import ListUtils (rsortOn)
+import Data.List (foldl')
 import qualified Data.Map as M
 import qualified Data.Set as S
 import System.Console.CmdArgs
 import System.Random
+import Text.Printf
 
 {-
 # Test various sizes
@@ -52,10 +55,27 @@ main = do
   let exp = Exp {
               noises = map (<=(1-noise options)) $ randomRs (0,1.0::Double) gen
             , rands = randomRs (False,True) gen2
-            , sampleTarget = S.fromList $ take (targetSize options) [1..]
+            , sampleTarget = target
             }
+      target = S.fromList $ take (targetSize options) [1..]
   st <- create exp sampler (take (size options) [1..])
-  putStrLn.show.run (divisorSearch (S.fromList [])) $ st
+  let res = run (searchSet (reps options)) $ st
+  printResults target res
+
+searchSet n = do
+  dss <- sequence (replicate n $ divisorSearch (S.fromList []))
+  return.foldl' (M.unionWith (+)) M.empty $ dss
+
+printResults expected actual = do
+  putStrLn.show $ expected
+  putStrLn.show $ actual
+  let actual' = S.fromList.map fst.take (S.size expected).rsortOn snd.
+                M.toList.M.delete reserved $ actual
+      missing = S.size.S.difference expected $ actual'
+      extra = S.size.S.difference actual' $ expected
+  printf "Number of missing elements: %i\n\
+         \Number of extra elements: %i\n"
+         missing extra
 
 sampler :: Exp -> S.Set Int -> S.Set Int -> (Exp,MDist Int)
 sampler expData idn a =
@@ -75,6 +95,14 @@ noisify dat ps =
   in if p == True
      then (dat',ps)
      else
-         let scale = if u then 0.2 else 0.3
-             ans = normalize.M.map (\p -> scale*p+scale) $ ps
+         let ans = M.fromList.uncurry zip.fmap (switch u).
+                   unzip.M.toList $ ps
          in (dat',ans)
+
+switch True (1:0:_) = [0.5,0.5]
+switch False (1:0:_) = [0.5,0.5]
+switch True (0:1:_) = [0.5,0.5]
+switch False (0:1:_) = [0.5,0.5]
+switch True (0.5:0.5:_) = [0.7,0.3]
+switch False (0.5:0.5:_) = [0.3,0.7]
+switch _ _ = error "MADNESS"
