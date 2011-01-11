767da16caf6e6893dc3e0347aa8886de4e5e6af7
Naren Sundaravaradan <nano.naren@gmail.com>
2011-01-11 10:53:46 +0800

    coprimes are now split randomly.

--- src/SetUtils.hs
+++ src/SetUtils.hs
@@ -10,15 +10,17 @@ import Test.Framework (defaultMain,testGroup)
 import Test.Framework.Providers.QuickCheck2 (testProperty)
 import Test.QuickCheck
 
-randCoprimeFactors :: Ord a => [Bool] -> S.Set a -> S.Set a ->
-                      ([Bool],Maybe (S.Set a,S.Set a))
+randCoprimeFactors :: Ord a => [Double] -> S.Set a -> S.Set a ->
+                      ([Double],Maybe (S.Set a,S.Set a))
 randCoprimeFactors ps iden d
     | diffsz == 1 = (ps,Nothing)
     | otherwise = (ps',Just (p1,p2))
     where diff = S.difference iden d
-          diffsz = S.size diff
-          (rands,ps') = splitAt diffsz ps
-          (pa,pb) = partition' fst (zip rands (S.toList diff))
+          diffsz = fromIntegral $ S.size diff
+          (r:rands,ps') = splitAt (S.size diff+1) ps
+          takeSize = ceiling (r*diffsz)
+          bools = map ((<=takeSize).floor.(*diffsz)) rands
+          (pa,pb) = partition' fst (zip bools (S.toList diff))
           p1 = S.union d $ S.fromList $ map snd pa
           p2 = S.union d $ S.fromList $ map snd pb
           partition' f xs
@@ -31,18 +33,18 @@ tests =
     [
      testGroup "Coprime tests"
        [
-        testProperty "empty" prop_coprime_empty
-       ,testProperty "nonempty" prop_coprime_nonempty
+        testProperty "nonempty" prop_coprime_nonempty
+       ,testProperty "empty" prop_coprime_empty
        ]
     ]
 
 prop_coprime_nonempty =
-    forAll (choose (1,49)) $ \divSize ->
-    forAll (vectorOf (51-divSize) $ choose (False,True)) $ \ps ->
+    forAll (choose (0,40)) $ \divSize ->
+    forAll (vectorOf 60 $ choose (0,1)) $ \ps ->
     let iden = S.fromList [0..50::Int]
         d = S.fromList.take divSize $ [0..50]
-        (ps',Just (x,y)) = randCoprimeFactors ps iden d
-    in null ps' && S.union x y == iden && S.intersection x y == d &&
+        (_,Just (x,y)) = randCoprimeFactors ps iden d
+    in S.union x y == iden && S.intersection x y == d &&
        x /= d && y /= d
 
 prop_coprime_empty ps =
