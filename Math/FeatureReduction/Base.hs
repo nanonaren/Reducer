8ea8567a7e01f16f21c3e0022170ee55afe28f35
Naren Sundaravaradan <nano.naren@gmail.com>
2011-04-20 18:21:01 -0400

    Level 1 code with test.

--- Math/FeatureReduction/Base.hs
+++ Math/FeatureReduction/Base.hs
@@ -1,14 +1,21 @@
+{-# LANGUAGE TupleSections #-}
 module Math.FeatureReduction.Base
     (
-      phiToPsi
+      FeatureInfo (..)
+    , Value
+    , phiToPsi
+    , level1
     ) where
 
 import Control.Monad.State
+import Data.List (partition,intersect)
+import Data.Functor.Identity
 import Math.FeatureReduction.Features
+import NanoUtils.Tuple
 
 data FeatureInfo = FeatureInfo
     {
-      x :: Int
+      allFS :: [Int]
     }
 
 type St = StateT FeatureInfo
@@ -25,3 +32,11 @@ phiToPsi phi =
     \f1 f2 -> do
         inf <- liftM2 (-) (phi f1) $ phi (diff f1 f2)
         return $ if inf < 0 then 0 else inf
+
+-- |Level 1
+level1 :: Monad m => Psi m -> St m (Features,Features)
+level1 psi = do
+  allfs <- gets allFS
+  let fs = fromList allfs
+  xs <- lift.mapM (\f -> liftM (f,).psi fs.fromList.(:[]) $ f) $ allfs
+  return.mapHomTup (fromList.map fst).partition ((>0).snd) $ xs
--- Math/FeatureReduction/TBase.hs
+++ Math/FeatureReduction/TBase.hs
@@ -6,15 +6,17 @@ module Math.FeatureReduction.TBase
 import Math.FeatureReduction.Features
 import Math.FeatureReduction.Base
 import Data.Functor.Identity
+import Control.Monad.State
 import qualified Data.BitSet as B
-import Data.List (sort,nub,(\\))
-import Test.QuickCheck
+import Data.List (sort,nub,(\\),intersect)
+import Test.QuickCheck hiding (sample)
 import Test.QuickCheck.Test
 
-testBase =
-    fmap and $
-    mapM (fmap isSuccess.quickCheckResult)
-             [prop_phiToPsi,prop_phiToPsi_zeroBound]
+testBase = do
+  xs <- mapM (fmap isSuccess.quickCheckResult)
+        [prop_phiToPsi,prop_phiToPsi_zeroBound]
+  o <- runOnce test_level1
+  return.and $ o:xs
 
 genInts = fmap (sort.nub) $ listOf (choose (1,100))
 
@@ -30,3 +32,13 @@ prop_phiToPsi_zeroBound = do
   let psi = phiToPsi (return.(1/).(+1).fromIntegral.B.size)
   forAll (genInts >>= \xs -> genInts >>= \ys -> return (xs,ys)) $ \(xs,ys) ->
       runIdentity (psi (fromList xs) (fromList ys)) >= 0
+
+test_level1 = runIdentity (evalStateT (level1 samplePsi) sample) ==
+              (fromList [2,5,7,9],fromList [0,1,3,4,6,8,10])
+
+runOnce = fmap isSuccess.quickCheckWithResult (stdArgs{maxSuccess=1})
+
+sample = FeatureInfo {allFS = [0..10]}
+samplePhi :: Features -> Identity Value
+samplePhi = return.fromIntegral.length.intersect [2,5,7,9].toList
+samplePsi = phiToPsi samplePhi
