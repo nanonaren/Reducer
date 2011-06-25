890e310babd5aaaac2292cfd00a9a6197d0c3521
Naren Sundaravaradan <nano.naren@gmail.com>
2011-06-25 13:50:50 -0400

    Removing old files

--- level2.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-./nnls 6991 > output/level2/6991
-./nnls 7003 > output/level2/7003
-./nnls 7063 > output/level2/7063
-./nnls 7064 > output/level2/7064
-./nnls 10805 > output/level2/10805
-./nnls 10158 > output/level2/10158
-./nnls 10160 > output/level2/10160
