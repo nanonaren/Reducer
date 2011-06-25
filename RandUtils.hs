85276774172361b56adbe9cc04c8f3cd871b0c2a
Naren Sundaravaradan <nano.naren@gmail.com>
2011-06-25 13:55:37 -0400

    More file removals

--- RandUtils.hs
+++
@@ -1,8 +0,0 @@
-module RandUtils
-    (
-      chooseInt
-    ) where
-
-chooseInt :: Integral a => Double -> a -> a -> a
-chooseInt p minB maxB = minB + floor (p*len)
-    where len = fromIntegral $ (maxB - minB) + 1
