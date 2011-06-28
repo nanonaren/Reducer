d38c1aab9aa5bfccc6e4f76086ee1909bf8a7dff
Naren Sundaravaradan <nano.naren@gmail.com>
2011-06-28 09:10:45 -0400

    using local paths

--- LCASt.hs
+++ LCASt.hs
@@ -81,15 +81,15 @@ type St = StateT LCA IO
 opts = Options
   {
     maxFits = 198 &= help "Number of impact factors"
-  , dataFile = "/home/narens/work/chinese/data/allnodes.tab" &= help "Data file" &= typFile
+  , dataFile = "data/allnodes.tab" &= help "Data file" &= typFile
   , nnlsPath = "/home/narens/Downloads/nnls" &= help "NNLS library path" &= typDir
   , rscriptPath = "/usr/bin/Rscript" &= help "Rscript path" &= typFile
   , rootNode = def &= help "Root node" &= typ "INT"
   , delta = 0.15 &= help "Allowable fit error" &= typ "DOUBLE"
-  , mustInclude = "/home/narens/work/chinese/data/includenodes" &= help "Must include nodes" &= typFile
-  , mustIncludeCat = "/home/narens/work/chinese/data/includecat" &= help "Must include categories" &= typFile
+  , mustInclude = "data/includenodes" &= help "Must include nodes" &= typFile
+  , mustIncludeCat = "data/includecat" &= help "Must include categories" &= typFile
   , numRandomNodes = 0 &= help "Number of extra random nodes to include" &= typ "INT"
-  , namesFile = "/home/narens/work/lca/data/allnames.tab" &= help "Node names file" &= typFile
+  , namesFile = "data/allnames.tab" &= help "Node names file" &= typFile
   , measure = enum [FitMeasure &= help "Use fits to measure closeness",
                     DistMeasure &= help "Use distance to measure closeness"]
   , knownChildren = def &= help "Known children"
